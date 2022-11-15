a5642aad7a2409363a644df3b3fa79e38467bf92
jovana <jmitic001@converse.edu>
2022-11-15 12:25:58 -0500

    Homework5

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
+const { query } = require('express');
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express', FormData:{}});
+});
+
+router.post('/', function(req, res, next) {
+  console.log(req.body);
+  res.render('index', { title: 'Express', FormData: req.body});
 });
 
 module.exports = router;
